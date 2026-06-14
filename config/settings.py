476154974ecd58ad204cf4c1f88ae60bf477598b
Olimjon0511 <xikmatullayevolimjon@gmail.com>
2026-06-14 16:56:46 +0500

    Add patients app: Patient model and CRUD

--- config/settings.py
+++ config/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'catalog',
     'clinics',
     'doctors',
+    'patients',
 ]
 
 MIDDLEWARE = [
---
+++ patients/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from users.models import User
+
+class Patient(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    birth_date = models.DateField()
+    address = models.CharField(max_length=225, blank=True)
+    
+
