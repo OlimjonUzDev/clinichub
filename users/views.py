415c69f3b8a111ac2b344394af1e1d4f6fc32d27
Olimjon0511 <xikmatullayevolimjon@gmail.com>
2026-06-19 08:20:25 +0500

    Add IsAdmin permission, secure RegisterView and DashboardView

--- users/views.py
+++ users/views.py
@@ -1,20 +1,24 @@
 from rest_framework import generics
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
 
 from .models import User
 from .serializers import RegisterSerializers
 from doctors.models import Doctor
 from patients.models import Patient
 from appointments.models import Appointment
+from .permissions import IsAdmin
 
 
 
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = RegisterSerializers
+    permission_classes = [AllowAny]
 
 class DashboardView(APIView):
+    permission_classes = [IsAdmin]
     def get(self, request):
         return Response({
             'total_doctors': Doctor.objects.count(),
